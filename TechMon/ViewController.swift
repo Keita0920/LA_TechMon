3e783931aebd07d7012d2586d83db3cc877fcf2f
Keita0920 <cy20255@shibaura-it.ac.jp>
2022-09-04 06:11:05 +0900

    final

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by K I on 2022/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
